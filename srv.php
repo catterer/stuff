5528fb31ed1f9124dc83448804a2592f4c595948
catterer <catterer@yandex.ru>
2015-12-16 16:41:35 +0300

    util

--- srv.php
+++ srv.php
@@ -1,20 +1,25 @@
 #!/usr/bin/env php
-
 <?php
 require 'vendor/autoload.php';
+require 'util.php';
 
-$app = function ($request, $response) {
+function main()
+{
+    $loop = React\EventLoop\Factory::create();
+    $socket = new React\Socket\Server($loop);
+    $http = new React\Http\Server($socket, $loop);
+
+    $http->on('request', function ($request, $response) {
+        hexdump($request->getQuery());
         $response->writeHead(200, array('Content-Type' => 'text/plain'));
-            $response->end("Hello World\n");
-};
+        $response->end("Hello World\n");
+    });
 
-$loop = React\EventLoop\Factory::create();
-$socket = new React\Socket\Server($loop);
-$http = new React\Http\Server($socket, $loop);
+    echo "Server running at http://127.0.0.1:1337\n";
 
-$http->on('request', $app);
-echo "Server running at http://127.0.0.1:1337\n";
+    $socket->listen(1337);
+    $loop->run();
+}
 
-$socket->listen(1337);
-$loop->run();
+main();
 ?>
